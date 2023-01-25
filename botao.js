ca8d35cfd78937c4585bd60c72a4a11b623c812d
gabrielferraribarros <111796630+gabrielferraribarros@users.noreply.github.com>
2023-01-25 16:52:24 -0400

    Add files via upload
    descricao de imagens e botao finalizados

--- botao.js
+++ botao.js
@@ -1,11 +1,16 @@
-const botao = document.querySelector(".btn-fixed");
+let mybutton = document.getElementById("myBtn");
 
-window.addEventListener("scroll", function (event) {
+window.onscroll = function() {scrollFunction()};
 
-    if (window.scrollY == 0){
-        botao.classList.remove("visible");
-    }else{
-        botao.classList.add("visible");
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-})
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
